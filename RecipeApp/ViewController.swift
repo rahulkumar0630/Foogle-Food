7865ee88595003cbda5c8a6a550a88646bff3e3e
Rahul-Kumar-Git <rahulkumar0650@gmail.com>
2017-06-17 16:37:34 -0500

    Added Server Directory

--- RecipeApp/ViewController.swift
+++ RecipeApp/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var WebView: UIWebView!
+    var currentURL:String = ""
+    
+    @IBOutlet var Orderbutton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Orderbutton.layer.cornerRadius = 10
+        
+        
+        let url = URL(string: "https://www.google.com")
+        let request = URLRequest(url: url!)
+        
+        WebView.loadRequest(request)
+        
+        
     }
 
+    @IBAction func OnOrderPress(_ sender: Any) {
+    
+        currentURL = (WebView.request?.url?.absoluteString)!
+        print(currentURL)
+
+    }
+    
+    
+    @IBAction func OnBackPress(_ sender: Any) {
+        WebView.goBack()
+    }
+    
+    
+    @IBAction func OnForwardPress(_ sender: Any) {
+        WebView.goForward()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +55,3 @@ class ViewController: UIViewController {
 
 
 }
-
