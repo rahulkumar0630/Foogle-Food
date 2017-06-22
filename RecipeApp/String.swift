736b6bfa0f68e52b8ff9b40016f13f277916d27d
Rahul-Kumar-Git <rahulkumar0650@gmail.com>
2017-06-22 10:49:22 -0500

    Can now take the price out of the JS

---
+++ RecipeApp/String.swift
@@ -0,0 +1,57 @@
+//
+//  String.swift
+//  RecipeApp
+//
+//  Created by Rahul Kumar on 6/20/17.
+//  Copyright © 2017 Rahul Kumar. All rights reserved.
+//
+
+//
+//  String.swift
+//  Hackin the Web
+//
+//  Created by Rahul Kumar on 6/20/17.
+//  Copyright © 2017 Kyle Lee. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func index(of string: String, options: CompareOptions = .literal) -> Index? {
+        return range(of: string, options: options)?.lowerBound
+    }
+    func endIndex(of string: String, options: CompareOptions = .literal) -> Index? {
+        return range(of: string, options: options)?.upperBound
+    }
+    func indexes(of string: String, options: CompareOptions = .literal) -> [Index] {
+        var result: [Index] = []
+        var start = startIndex
+        while let range = range(of: string, options: options, range: start..<endIndex) {
+            result.append(range.lowerBound)
+            start = range.upperBound
+        }
+        return result
+    }
+    func ranges(of string: String, options: CompareOptions = .literal) -> [Range<Index>] {
+        var result: [Range<Index>] = []
+        var start = startIndex
+        while let range = range(of: string, options: options, range: start..<endIndex) {
+            result.append(range)
+            start = range.upperBound
+        }
+        return result
+    }
+    subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = index(startIndex, offsetBy: r.lowerBound)
+        let end = index(startIndex, offsetBy: r.upperBound - r.lowerBound)
+        return self[Range(start ..< end)]
+    }
+}
--- RecipeApp/ViewController.swift
+++ RecipeApp/ViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
     @IBOutlet var WebView: UIWebView!
     var currentURL:String = ""
+    let webViewforData = WKWebView()
+    var stringforvalue:String = ""
+    var boolForSubstring:Bool = false
+    var incremetor:Int = 0
+    var newPriceString:String = ""
+    var NextQoute: Bool = false
+    var UrlBeforeSent: String = ""
+    var StringforIngredients: String = ""
+    var Price:Double = 0.0
+
     
     @IBOutlet var Orderbutton: UIButton!
     
@@ -23,17 +34,184 @@ class ViewController: UIViewController {
         
         
         let url = URL(string: "https://www.google.com")
+
         let request = URLRequest(url: url!)
         
         WebView.loadRequest(request)
         
+        webViewforData.navigationDelegate = self
+        
+    }
+    
+    
+    
+    
+    func FindIngredientPrice(Ingredient: String){
+        
+        //print("http://ec2-13-58-166-251.us-east-2.compute.amazonaws.com/test.html?Data=\(Ingredient)")
+
+        self.UrlBeforeSent = "http://ec2-13-58-166-251.us-east-2.compute.amazonaws.com/test.html?Data=\(Ingredient)"
+        //print(self.UrlBeforeSent)
+        
+        self.UrlBeforeSent = self.UrlBeforeSent.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
+        print(self.UrlBeforeSent)
+        
+        let foodPriceUrl = URL(string: self.UrlBeforeSent)
+
+        let requestforPrice = URLRequest(url: foodPriceUrl!)
+        self.webViewforData.load(requestforPrice)
+        
+        let when = DispatchTime.now() + 2 // change 2 to desired number of seconds
+        DispatchQueue.main.asyncAfter(deadline: when) {
+        
+        self.webViewforData.evaluateJavaScript("document.getElementsByTagName('html')[0].innerHTML", completionHandler: { (value, error) in
+            //print(value)
+            self.stringforvalue = value as! String
+            
+            if self.stringforvalue.range(of:"price:") != nil{
+            
+                
+                let range: Range<String.Index> = self.stringforvalue.range(of: "price:")!
+                let index: Int = self.stringforvalue.distance(from: self.stringforvalue.startIndex, to: range.lowerBound)
+                self.incremetor = index
+                
+                
+                while self.boolForSubstring == false
+                {
+                    if(self.stringforvalue[self.incremetor] != "\"")
+                    {
+                        self.incremetor = self.incremetor + 1
+                    }
+                    if(self.stringforvalue[self.incremetor] == "\"")
+                    {
+                        //self.newPriceString = self.newPriceString + self.stringforvalue[self.incremetor]
+                        self.NextQoute = true
+                        self.incremetor = self.incremetor + 2
+                        
+                        while self.boolForSubstring == false
+                        {
+                            
+                            if(self.stringforvalue[self.incremetor] != "\"")
+                            {
+                                self.newPriceString = self.newPriceString + self.stringforvalue[self.incremetor]
+                                self.incremetor = self.incremetor + 1
+                            }
+                            else
+                            {
+                                self.boolForSubstring = true
+                            }
+                        }
+                    }
+                }
+                //self.Price = Double(self.newPriceString)!
+                print(self.newPriceString)
+                self.newPriceString = ""
+                self.boolForSubstring = false
+                self.NextQoute = false
+                self.incremetor = 0
+                self.UrlBeforeSent = ""
+                
+            }
+            
+        })
+            
+        }
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Loaded")
         
     }
 
     @IBAction func OnOrderPress(_ sender: Any) {
+        
+        //self.FindIngredientPrice(Ingredient: "3 pounds beef")
     
         currentURL = (WebView.request?.url?.absoluteString)!
         print(currentURL)
+        var ArrayForIngredients = [String]()
+        
+        let uRl = URL(string: "http://ec2-13-58-166-251.us-east-2.compute.amazonaws.com/SERVER/index.php?url=" + currentURL)
+        
+        let task = URLSession.shared.dataTask(with: uRl!) { (data, response, error) in
+            
+            
+            if error != nil {
+                print("Error")
+            }
+            else {
+                if let mydata = data {
+                    do {
+                        
+                        let myJson = try JSONSerialization.jsonObject(with: mydata, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        
+                        if let stations = myJson["extendedIngredients"] as? [[String: AnyObject]] {
+                            
+                            for station in stations {
+                                
+                                if let name = station["originalString"] as? String{
+                                    
+                                   let aisle = station["aisle"] as? String
+                                   
+                                    
+                                   if(aisle == "?")
+                                   {
+                                      print("This is not a recipe")
+                                      break
+                                   }
+                                   var contructedstring = ""
+                                    
+                                   if let amount = station["amount"] as? Double
+                                   {
+                                      contructedstring = "\(amount)"
+                                   }
+                                   if let unitLong = station["unitLong"] as? String
+                                   {
+                                      contructedstring = contructedstring + " \(unitLong)"
+                                   }
+                                   if let name = station["name"] as? String
+                                   {
+                                      contructedstring = contructedstring + " \(name)"
+                                   }
+                                
+                                   print(contructedstring)
+                                   ArrayForIngredients.append(contructedstring)
+                                    
+                                }
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    catch {
+                        // catch error
+                    }
+                }
+            }
+            
+            for item in ArrayForIngredients {
+                
+                if(item == ArrayForIngredients[ArrayForIngredients.count - 1])
+                {
+                    self.StringforIngredients = "\(self.StringforIngredients) \(item)."
+                    break
+                }
+                self.StringforIngredients = "\(self.StringforIngredients) \(item) |"
+            }
+            self.StringforIngredients = self.StringforIngredients.trimmingCharacters(in: .whitespacesAndNewlines)
+            print(self.StringforIngredients)
+            self.FindIngredientPrice(Ingredient: self.StringforIngredients)
+            self.StringforIngredients = ""
+            
+        }
+        
+        task.resume()
+        
+        
+        
 
     }
     
